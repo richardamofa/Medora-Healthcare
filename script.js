06863497f0199d2516f742340652cd22efa70885
richie-codesxp <amofarichard101@gmail.com>
2026-01-14 18:07:48 +0000

    responsiveness

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("open");
+  navLinks.classList.toggle("active");
+});
+
+
 const navbar = document.querySelector(".navbar");
 
 window.addEventListener("scroll", () => {
